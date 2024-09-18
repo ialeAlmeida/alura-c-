1044a1606101e2fd002351373254cc7fe00f5cca
ialeAlmeida <almeidaialesantos@gmail.com>
2024-09-17 23:34:11 -0300

    WIP: added: exercises from third module

--- PrimeiraAplicacao/Program.cs
+++ PrimeiraAplicacao/Program.cs
@@ -1,85 +1,139 @@
-﻿string mensagemDeBoasVindas = "Bem vinda a sua biblioteca de músicas online :)";
-List<string> bandas = new List<string>();
+﻿//Exercícios do módulo 03
 
-//Função
-void ExibirLogo()
+using System.Runtime.Serialization;
+
+void Contas(double a, double b)
 {
-    Console.WriteLine(@"
-░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
-██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
-╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
-░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
-██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
-╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");    
-    Console.WriteLine($"\n{mensagemDeBoasVindas}");
-}
+    double adicao = a + b;
+    double subtracao = a - b;
+    double multiplicacao = a * b;
+    double divisao = a / b;
+    double divisao2 = b / a;
 
-void ExibirOpcoesDoMenu()
-{
-    Console.WriteLine("""
+    Console.WriteLine($"""
+        Número a = {a}
+        Número b = {b}
 
-        Menu
-        1 - Registrar banda
-        2 - Mostrar sua lista de bandas
-        3 - Avaliar uma banda
-        4 - Exibir a média de uma banda
-        0 - Para sair
+        Adição = {adicao}
+        Subtração = {subtracao}
+        Multiplicação = {multiplicacao}
+        Divisão de {a} por {b} = {divisao}
+        Divisão de {b} por {a} = {divisao2}
         """);
-
-    Console.Write("Digite sua opção: ");
-    //Ler a resposta e armazena
-    //! declara que o valor não pode ser nulo
-    string respostaMenu = Console.ReadLine()!;
-    int respostaMenuNum = int.Parse(respostaMenu);
-
-    switch(respostaMenuNum)
-    {
-        case 1: 
-            RegistrarBanda();
-            break;
-        case 2: 
-            MostrarBandas();
-            break;
-        case 3: 
-            //AvaliarBanda();
-            break;
-        case 4: 
-            //ExibirMediaBanda();
-            break;
-     }
 }
 
-void FinalFuncao()
+Contas(6, 3);
+
+Console.WriteLine("\n*********************************************\n");
+
+List<string> cantorasPrediletas = new List<string>();
+cantorasPrediletas.Add("Mc Luanna");
+cantorasPrediletas.Add("Duquesa");
+cantorasPrediletas.Add("Liniker");
+
+for(int i = 0; i < cantorasPrediletas.Count; i++)
 {
-    Thread.Sleep(2500);
-    Console.Clear();
-    ExibirOpcoesDoMenu();
-}
-void RegistrarBanda()
-{
-    Console.Clear();
-    Console.WriteLine("Registro de bandas");
-    Console.Write("Digite o nome da banda que você quer registrar: ");
-    string nomeBanda = Console.ReadLine()!;
-    bandas.Add(nomeBanda);
-    Console.WriteLine($"A banda {nomeBanda} foi registrado com sucesso!");
-    FinalFuncao();
-}
- 
-void MostrarBandas()
-{
-    Console.Clear();
-    Console.WriteLine("Lista de bandas\n");
-    foreach (var banda in bandas)
-    {
-        Console.WriteLine(banda);
-    }
-    //Outra forma de imprimir(/correr) uma lista
-    //for (int i = 0; i < bandas.Count; i++) {
-    //Console.WriteLine(bandas[i]);
-    //}
+    Console.Write("Cantora: ");
+    Console.WriteLine(cantorasPrediletas[i]);
 }
 
-//Chamando função pra ser executada
-ExibirLogo();
-ExibirOpcoesDoMenu();
+Console.WriteLine("\n*********************************************\n");
+
+//List<int> nums = new List<int> { 34, 67, 9, 9 };
+//for (int i = 0; i < nums.Count; i++)
+//{
+//    int somaNums =+ nums[i];
+//    Console.WriteLine(somaNums);
+//}
+//foreach (int num in nums)
+//{
+//    int soma =+ nums[num];
+//    Console.WriteLine(soma);
+//}
+
+
+//string mensagemDeBoasVindas = "Bem vinda a sua biblioteca de músicas online :)";
+//List<string> bandas = new List<string>();
+
+////Função
+//void ExibirLogo()
+//{
+//    Console.WriteLine(@"
+//░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+//██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+//╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+//░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+//██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+//╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");    
+//    Console.WriteLine($"\n{mensagemDeBoasVindas}");
+//}
+
+//void ExibirOpcoesDoMenu()
+//{
+//    Console.WriteLine("""
+
+//        Menu
+//        1 - Registrar banda
+//        2 - Mostrar sua lista de bandas
+//        3 - Avaliar uma banda
+//        4 - Exibir a média de uma banda
+//        0 - Para sair
+//        """);
+
+//    Console.Write("Digite sua opção: ");
+//    //Ler a resposta e armazena
+//    //! declara que o valor não pode ser nulo
+//    string respostaMenu = Console.ReadLine()!;
+//    int respostaMenuNum = int.Parse(respostaMenu);
+
+//    switch(respostaMenuNum)
+//    {
+//        case 1: 
+//            RegistrarBanda();
+//            break;
+//        case 2: 
+//            MostrarBandas();
+//            break;
+//        case 3: 
+//            //AvaliarBanda();
+//            break;
+//        case 4: 
+//            //ExibirMediaBanda();
+//            break;
+//     }
+//}
+
+//void FinalFuncao()
+//{
+//    Thread.Sleep(2500);
+//    Console.Clear();
+//    ExibirOpcoesDoMenu();
+//}
+//void RegistrarBanda()
+//{
+//    Console.Clear();
+//    Console.WriteLine("Registro de bandas");
+//    Console.Write("Digite o nome da banda que você quer registrar: ");
+//    string nomeBanda = Console.ReadLine()!;
+//    bandas.Add(nomeBanda);
+//    Console.WriteLine($"A banda {nomeBanda} foi registrado com sucesso!");
+//    FinalFuncao();
+//}
+
+//void MostrarBandas()
+//{
+//    Console.Clear();
+//    Console.WriteLine("Lista de bandas\n");
+//    foreach (var banda in bandas)
+//    {
+//        Console.WriteLine(banda);
+//    }
+//    //Outra forma de imprimir(/correr) uma lista
+//    //for (int i = 0; i < bandas.Count; i++) {
+//    //Console.WriteLine(bandas[i]);
+//    //}
+//}
+
+////Chamando função pra ser executada
+//ExibirLogo();
+//ExibirOpcoesDoMenu();
--- PrimeiraAplicacao/obj/Debug/net8.0/PrimeiraAplicacao.AssemblyInfo.cs
+++ PrimeiraAplicacao/obj/Debug/net8.0/PrimeiraAplicacao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiraAplicacao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4ee4e8c16e349f4d7bf4575a82cfe221a1d5b13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2b20f229198b80844239048532d529173a6bd31")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiraAplicacao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiraAplicacao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
